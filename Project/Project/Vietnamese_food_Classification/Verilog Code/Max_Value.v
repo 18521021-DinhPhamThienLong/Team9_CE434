11b661a51ab09ef010e13cd0a68f74e4079f14da
Diem Ngoc <70463525+18520597-DiemNgoc@users.noreply.github.com>
2021-06-09 21:59:52 +0700

    Update Max_Value.v

--- Project/Project/Vietnamese_food_Classification/Verilog Code/Max_Value.v
+++ Project/Project/Vietnamese_food_Classification/Verilog Code/Max_Value.v
@@ -1,10 +1,9 @@
-module Max_Value(max_value, In1, In2);
-	input [31:0] In1, In2;
-	output reg [31:0] max_value;
+module Max_Value #(parameter data_width = 32)(max_value, In1, In2);
+	input [data_width - 1:0] In1, In2;
+	output reg [data_width - 1:0] max_value;
 
 	always @(In1, In2) begin
 		if(In1 > In2) max_value = In1;
 		else max_value = In2;
 	end
 endmodule
-// so chu ky : 1
