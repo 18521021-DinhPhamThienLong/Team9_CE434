a719270b96af4cca19f741fafe2d736233a1db07
Diem Ngoc <70463525+18520597-DiemNgoc@users.noreply.github.com>
2021-06-12 00:31:09 +0700

    Update ReLU_tb.v

--- Project/Project/Vietnamese_food_Classification/Verilog Code/ReLU_tb.v
+++ Project/Project/Vietnamese_food_Classification/Verilog Code/ReLU_tb.v
@@ -4,16 +4,21 @@ module ReLU_tb();
 	parameter t = 10;
 	parameter data_width = 32;
 
+	reg clk;
 	reg signed[data_width - 1: 0] data_in;
 	wire[data_width - 1: 0] out_ReLU;
 
 	initial begin
+		clk <= 'b0;
 		#t data_in <= 'b1100_0001_0010_0110_0110_0110_0110_0110; // -10.4
 		#t data_in <= 'b0100_0000_1011_0011_0011_0011_0011_0011; // 5.6
 		#t data_in <= 'b0000_0000_0000_0000_0000_0000_0000_0010; // 2
 		#(t*3) $finish;
 	end
 
-	ReLU #(data_width) DUT(.out_ReLU(out_ReLU), .data_in(data_in));
+	always @(clk)
+   		 #(t/2) clk <= ~clk;
+
+	ReLU #(data_width) DUT(.out_ReLU(out_ReLU), .data_in(data_in), .clk(clk));
 
 endmodule
