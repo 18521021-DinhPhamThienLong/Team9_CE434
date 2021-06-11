c2bf53753cfb1712c2b8801827c9eb7b4492cf49
Diem Ngoc <70463525+18520597-DiemNgoc@users.noreply.github.com>
2021-06-11 14:47:13 +0700

    Update Max_Data.v

--- Project/Project/Vietnamese_food_Classification/Verilog Code/Max_Data.v
+++ Project/Project/Vietnamese_food_Classification/Verilog Code/Max_Data.v
@@ -1,11 +1,34 @@
-module Max_Data(max_data, In1, In2, In3, In4);
-	input[31:0] In1, In2, In3, In4;
-	output[31:0] max_data;
+/*
+In: day so co do dai bang (data_width*R*C), với cu data_width bit tuong ung la 1 so
+Muc dich: tim so lon nhat trong tap hop so duoc luu
+*/
+module Max_Data #(parameter data_width = 32, R = 3, C = 3)(max_data, In);
+	input[data_width*R*C:0] In;	// tat ca gia tri pixel can xet, 1 pixel tuong ung 32 bit
+	output[data_width-1:0] max_data;
 	
-	wire[31:0] max1, max2;
+	wire[data_width-1:0] reg_in[0:(R*C)-1];	// noi luu tru gia tri tung pixel dau vao
+	wire[data_width-1:0] reg_out[0:(R*C)-1];	// noi luu tru gia tri tung pixel dau ra
 	
-	Max_Value MAX1(max1, In1, In2);
-	Max_Value MAX2(max2, In3, In4);
-	Max_Value DUT(max_data, max1, max2);
+	// tien hanh luu tru input
+	genvar i;
+	generate
+		// chay vong for tu dau den pixel cuoi cung
+		// 32bit la 1 pixel nen i = i + data_width
+		for(i = 0; i <(data_width*R*C); i = i + data_width) begin
+			// do buoc nhay i = data_width nen reg_in[i/data_width]
+			assign reg_in[i/data_width] = In[i + data_width:i];	
+		end 	
+	endgenerate
+	
+	genvar j;
+	generate
+		assign reg_out[0] = reg_in[0]; 
+		for(j = 0; j < (R*C) - 1; j = j + 1) begin
+			Max_Value #(data_width) DUT(.max_value(reg_out[j+1]), .In1(reg_out[j]), .In2(reg_in[j+1]));
+		end
+		
+		assign max_data = reg_out[(R*C) - 1];
+	
+	endgenerate
 	
 endmodule
