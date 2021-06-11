e6c4a8a1815d2a35c39caa4cfc99a53648268bb1
Diem Ngoc <70463525+18520597-DiemNgoc@users.noreply.github.com>
2021-06-12 00:30:33 +0700

    Update ReLU.v

--- Project/Project/Vietnamese_food_Classification/Verilog Code/ReLU.v
+++ Project/Project/Vietnamese_food_Classification/Verilog Code/ReLU.v
@@ -1,11 +1,15 @@
 /*
 Muc dich: neu input la so am thi xuat ra 0
 */
-module ReLU #(parameter data_width = 32)(out_ReLU, data_in);
+module ReLU #(parameter data_width = 32)(out_ReLU, data_in, clk);
+	input clk;
 	input signed[data_width - 1:0] data_in;
-	output[data_width - 1:0] out_ReLU;
+	output reg[data_width - 1:0] out_ReLU;
 	
-	assign out_ReLU = data_in[data_width - 1] ? 'd0 : data_in;
-	
+	always @(posedge clk) begin
+		if(data_in[data_width - 1])
+			out_ReLU <= 'd0;
+		else out_ReLU = data_in;
+	end
 endmodule
 
