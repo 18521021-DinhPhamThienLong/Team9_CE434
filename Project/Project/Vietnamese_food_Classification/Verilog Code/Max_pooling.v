21c94cb9cb80fcae73caf873a497a1ae00a170fc
Diem Ngoc <70463525+18520597-DiemNgoc@users.noreply.github.com>
2021-06-25 17:04:11 +0700

    update Max Pooling with reset signal low

--- Project/Project/Vietnamese_food_Classification/Verilog Code/Max_pooling.v
+++ Project/Project/Vietnamese_food_Classification/Verilog Code/Max_pooling.v
@@ -7,9 +7,9 @@ module Max_pooling #(parameter data_width = 32, width = 5)(data_out, valid_out, 
 	wire[data_width - 1:0] data_out0, data_out1, data_out2, data_out3;
 	wire valid_op;
 	
-	LineBuff #(data_width, width) Buff(.data_out0(data_out0), .data_out1(data_out1), .data_out2(data_out2), .data_out3(data_out3), .valid_out(valid_op), .data_in(data_in), .clk(clk), .reset(reset), .valid_in(valid_in));
+	LineBuff #(data_width, width) Buff(.data_out0(data_out0), .data_out1(data_out1), .data_out2(data_out2), .data_out3(data_out3), .valid_out(valid_op), .data_in(data_in), .clk(clk), .reset(~reset), .valid_in(valid_in));
 
-	Max_Data #(data_width) DUT(.max_data(data_out), .valid_out(valid_out), .In1(data_out0), .In2(data_out1), .In3(data_out2), .In4(data_out3), .clk(clk), .reset(reset), .valid_in(valid_op));
+	Max_Data #(data_width) DUT(.max_data(data_out), .valid_out(valid_out), .In1(data_out0), .In2(data_out1), .In3(data_out2), .In4(data_out3), .clk(clk), .reset(~reset), .valid_in(valid_op));
 	
 endmodule
  
@@ -28,83 +28,91 @@ module LineBuff#(parameter data_width = 32, width = 4)(data_out0, data_out1, dat
 	reg[data_width - 1:0] cout, count;
 	
 	genvar i;
-	generate
-	for(i = 0; i < din; i = i + 1) begin
-		always @(posedge clk) begin
-			if(valid_in) begin
-				if(i == 0) line_buff[i] = data_in;
-				else line_buff[i] = line_buff[i - 1];
-			end
+  	generate 
+      for (i=0; i < din; i = i + 1)begin
+          always @(posedge clk) begin
+              if (valid_in)begin
+                	if(i == 0)
+                    	line_buff[i] <= data_in;    
+                	else
+                    	line_buff[i] <= line_buff[i-1];
+            	end
+        	end
 		end
-	end
-	endgenerate
-	
-	always @(*) begin 
-		if(reset) begin
-			s <= 'b01;
-		end
-		else s <= next_stage;
-	end
-	
-	always @(posedge clk) begin
-		if(reset) begin
-			valid_out <= 'b0;
-			count <= 'd0;
-			cout <= 'd0;
-		end
-		else if(valid_in) begin
-		case(s)
-		'b00: begin 
-			count <= 'd0;
-			cout <= 'd0;
-			valid_out <= 'b0;
-		end
-		'b01: begin 
-			count <= count + 1;
-			cout <= 'd0;
-			valid_out <= 'b0;
-		end
-		'b10: begin 
-			count <= 'd0;
-			cout = cout + 1;
-			valid_out <= 'b0;	
-		end
-		'b11: begin
-			count <= 'd0;
-			cout = cout +1;
-			valid_out <= 'b1;
-		end
-		endcase
-		end
-		else valid_out <= 'b0; // valid_in = 0
-	end
-	
+  	endgenerate
+  
+  	always @(*)begin
+    	if(reset)begin
+        	s  <= 'b01;
+    	end
+    	else begin
+        	s <= next_stage;
+      	end
+ 	end
+     
+  	always @(posedge clk)begin
+      	if(reset) begin
+        	count <= 'd0;
+        	cout <= 'd0;
+        	valid_out <= 0;
+    	end
+    	else if(valid_in)begin
+        case(s)
+        	'b00: begin
+            	count <= 'd0;
+                cout <= 'd0;
+                valid_out <= 0; 
+          	end 
+          	'b01: begin
+                count <= count + 1;
+                cout <= 'd0;
+                valid_out <= 0;
+         	end
+          	'b10: begin
+                count <= 'd0;
+                cout = cout + 1;
+                valid_out <= 0;
+          	end
+          	'b11: begin
+                count <= 'd0;
+                cout = cout + 1;
+                valid_out <= 1;
+          	end
+        endcase
+      	end
+      	else valid_out <= 0;
+    end
+    
 	always @(*) begin
-		case(s)
-		'b00: begin // khoi tao
-			if(valid_in) next_stage = 'b01;
-			else next_stage = 'b00;
-		end
-		'b01: begin // nhap
-			if(count == din - 1) next_stage = 'b10;
-			else next_stage = 'b01;
-		end
-		'b10: begin // kiem tra du dk xuat chua
-			if(cout % 2 == 0) next_stage = 'b11;
-			else if(cout == width - 1) next_stage = 'b01;
-			else next_stage = 'b10;
-		end
-		'b11: begin
-			if(cout % 2 == 1) next_stage = 'b10;
-			else if(cout == width - 1) next_stage = 'b01;
-			else next_stage = 'b11;
-		end
-		endcase
-	end
-	
+    	case(s)
+        'b00: begin
+            if(valid_in)
+                next_stage = 'b01;
+            else
+                next_stage = 'b00;
+        end
+        'b01: begin
+            if(count == din - 1)
+                next_stage = 'b10;
+            else
+                next_stage = 'b01;
+        end
+        'b10: begin
+            if(cout % 2 == 0) next_stage = 'b11;
+            else if(cout == width - 1 ) next_stage = 'b01;
+            else next_stage = 'b10;
+        end
+        'b11: begin
+            if(cout % 2 == 1)  next_stage = 'b10;
+           	else if(cout == width - 1 )  next_stage = 'b01;
+            else next_stage = 'b11;
+        end
+      endcase
+  	end  
+      
 	assign data_out0 = line_buff[0];
  	assign data_out1 = line_buff[1];
 	assign data_out2 = line_buff[din - 2];
 	assign data_out3 = line_buff[din- 1];
-	
+
 endmodule
