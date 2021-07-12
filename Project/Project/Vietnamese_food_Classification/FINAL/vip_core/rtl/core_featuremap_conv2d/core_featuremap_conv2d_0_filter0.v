39b9fea86ef82ab944ede62ae4db36ee27d4ef74
Dinh Pham Thien Long (18521021) <53068735+18521021-DinhPhamThienLong@users.noreply.github.com>
2021-07-12 14:34:23 +0700

    Add files via upload

---
+++ Project/Project/Vietnamese_food_Classification/FINAL/vip_core/rtl/core_featuremap_conv2d/core_featuremap_conv2d_0_filter0.v
@@ -0,0 +1,40 @@
+module core_featuremap_conv2d_0_filter0 (
+    clock,
+    reset,
+    // FIFO READ
+    ff_rdata,
+    ff_rdreq,
+    ff_empty,
+    // FIFO WRITE
+    ff_wdata,
+    ff_wrreq,
+    ff_full
+);
+//
+parameter  DWIDTH = 32;
+//
+input   clock;
+input   reset;
+// FIFO READ
+input [DWIDTH*3-1:0] ff_rdata;
+output               ff_rdreq;
+input                ff_empty;
+// FIFO WRITE
+output [DWIDTH-1:0]   ff_wdata;
+output                ff_wrreq;
+input                 ff_full;
+//
+
+featuremap_conv2d_0_filter0 inst(
+    .clk(clock),
+    .rst(reset), 
+
+    .data_in(ff_rdata),
+    .data_fifo_empty(ff_empty),
+
+    .data_out(ff_wdata),
+    .valid_out(ff_wrreq),
+    .rdreq(ff_rdreq)
+);
+
+endmodule
